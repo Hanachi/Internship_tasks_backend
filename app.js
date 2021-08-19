dbd712d00f3819b31243f8b4eb13665593f66f3d
Hanachi <yarko-pryima@ukr.net>
2021-08-19 10:00:52 +0300

    fix port

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.get('/movies', (req, res) => {
   })
 })
 
-const PORT  = process.env.PORT || 3000;
+const PORT  = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Listening on http://localhost:${PORT}`);
