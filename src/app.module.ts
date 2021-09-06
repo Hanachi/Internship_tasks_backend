0614685ccf92e640358b39a1d41c58f20cb1a2ed
Hanachi <yarko-pryima@ukr.net>
2021-09-06 10:09:11 +0300

    connected to DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MoviesModule } from './movies/movies.module';
 
+
 @Module({
-  imports: [MoviesModule],
+  imports: [MoviesModule, MongooseModule.forRoot('mongodb+srv://hanachi:tzCGQrqFIFqGVZwg@cluster0.udr3v.mongodb.net/Movies?retryWrites=true&w=majority')],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/movies/movies.module.ts
+++ src/movies/movies.module.ts
@@ -1,10 +1,13 @@
 import { Module } from "@nestjs/common";
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { MoviesController } from "./movies.controller";
 import { MoviesDataSource } from "./movies.provider";
 import { MoviesService } from "./movies.service";
+import { Movie, MovieSchema } from './schemas/movies.schema';
 
 @Module({
+	imports: [MongooseModule.forFeature([{ name: Movie.name, schema: MovieSchema }])],
 	providers: [MoviesService, MoviesDataSource],
 	controllers: [MoviesController]
 })
